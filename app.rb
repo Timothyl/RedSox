21dd47e69526452948d6da26a41a92bd099a7b51
Timothyl <timothyjlau@gmail.com>
2015-09-26 17:59:31 -0400

    finished!

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra/activerecord'
 require 'sinatra/reloader'
 require 'nokogiri'
 require 'open-uri'
+require 'httparty'
+require 'json'
 
 configure :development, :test do
   require 'pry'
@@ -18,8 +20,31 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
 end
 
 get '/' do
-  doc = Nokogiri::HTML(open('http://boston.redsox.mlb.com/schedule/?c_id=bos#y=2015&m=9&calendar=DEFAULT'))
-  a = doc.css('div')
-  binding.pry
-  erb :index
+  a = Party.schedule
+  t = Time.now.strftime("%-m/%d/%Y")
+  a = a['schedule_team_sponsors']['schedule_team_complete']['queryResults']['row']
+  i = a.index{|r| r['team_game_time'].include?(t)}
+  if i == nil
+    answer = "NO"
+  elsif
+    venue = a[i]['venue_name']
+    if venue == "Fenway Park"
+      answer = "YES"
+    else
+      answer = "NO"
+    end
+  end
+
+  # binding.pry
+  erb :index, locals: {answer: answer}
+end
+
+class Party
+  include HTTParty
+  base_uri 'http://boston.redsox.mlb.com/lookup/json/named.schedule_team_sponsors.bam?start_date=%272015/09/01%27&end_date=%272015/09/30%27&team_id=111&season=2015'
+
+  def self.schedule
+    get("")
+  end
+
 end
